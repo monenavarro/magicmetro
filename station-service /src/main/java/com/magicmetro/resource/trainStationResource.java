9e2bb1e560b2074de2160a7eb99abfcb741da940
Monica Navarro <me.navarro.97@outlook.com>
2022-12-12 14:04:02 +0000

    edit

--- station-service /src/main/java/com/magicmetro/resource/trainStationResource.java
+++ station-service /src/main/java/com/magicmetro/resource/trainStationResource.java
@@ -1,9 +1,24 @@
 package com.magicmetro.resource;
 
+import com.magicmetro.entity.TrainStation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class trainStationResource {
 
+@RestController
+public class TrainStationResource {
+
+    @Autowired
+    private com.magicmetro.service.TrainStationService TrainStationService;
+
+    @RequestMapping(path = "/stations/{sid}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public TrainStation searchTrainStationByIdResource(@PathVariable("sid") int id) {
+        return TrainStationService.searchTrainStationById(id);
+
+    }
 
 }
